4e5d10ce6544721958585deac94c71700cfb3db4
JongHyeon <sjh2273@naver.com>
2021-07-09 11:12:05 +0900

    first commit

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,6 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_URL = 'htps://api.themoviedb.org/3/';
+
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
--- server/config/prod.js
+++ server/config/prod.js
@@ -1,3 +1,5 @@
 module.exports = {
     mongoURI:process.env.MONGO_URI
 }
+
+//movie api : 07238924a74d749c3d6d82c74a2ceb2c
